611def53ef7d8a40764ea011a90e87db60d8d0c6
ytrm000 <ytrm000@yahoo.co.jp>
2021-02-06 22:01:04 +0900

    change App.js

--- expoPracticeApp/App.js
+++ expoPracticeApp/App.js
@@ -1,21 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import {
+  Provider as PaperProvider,
+  Text,
+  TextInput,
+  Button,
+  DefaultTheme,
+} from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3498db',
+    accent: '#f1c40f',
+    surface: 'green',
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          Keyboard.dismiss();
+        }}
+      >
+        <KeyboardAvoidingView style={styles.container} behavior="padding">
+          <Text style="title">ログイン</Text>
+          <TextInput
+            mode="outlined"
+            label="ユーザ名またはメールアドレス"
+            keyboardType="email-address"
+          ></TextInput>
+          <TextInput
+            mode="outlined"
+            label="パスワード"
+            secureTextEntry={true}
+          ></TextInput>
+          <Button
+            icon="login"
+            mode="contained"
+            onPress={() => {
+              Keyboard.dismiss();
+              console.log('Pressed');
+            }}
+          >
+            ログイン
+          </Button>
+          <StatusBar style="auto" />
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    textAlign: 'center',
+    alignItems: 'center',
   },
 });

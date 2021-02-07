686e3dc1aeae74a7ddda7b63270a1a32a60ab811
ytrm000 <ytrm000@yahoo.co.jp>
2021-02-07 19:42:07 +0900

    Fix App.js and add release build settings

--- reactNativeCliPracticeApp/App.js
+++ reactNativeCliPracticeApp/App.js
@@ -1,114 +1,69 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
-  SafeAreaView,
+  Keyboard,
+  KeyboardAvoidingView,
   StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+  TouchableWithoutFeedback,
 } from 'react-native';
-
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  Provider as PaperProvider,
+  Text,
+  TextInput,
+  Button,
+  DefaultTheme,
+} from 'react-native-paper';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3498db',
+    accent: '#f1c40f',
+    surface: 'green',
+  },
 };
 
+export default function App() {
+  return (
+    <PaperProvider theme={theme}>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          Keyboard.dismiss();
+        }}>
+        <KeyboardAvoidingView style={styles.container} behavior="padding">
+          <Text style="title">ログイン</Text>
+          <TextInput
+            mode="outlined"
+            label="ユーザ名またはメールアドレス"
+            keyboardType="email-address"
+          />
+          <TextInput
+            mode="outlined"
+            label="パスワード"
+            secureTextEntry={true}
+          />
+          <Button
+            mode="contained"
+            onPress={() => {
+              Keyboard.dismiss();
+              console.log('Pressed');
+            }}>
+            ログイン
+          </Button>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
+    </PaperProvider>
+  );
+}
+
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  title: {
+    textAlign: 'center',
+    alignItems: 'center',
   },
 });
-
-export default App;
